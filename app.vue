761a5fd0abcafdc785711a83a00e10922557d16e
aarizona <ar.ariz.arizona@gmail.com>
2024-02-21 15:10:57 +0300

    scss vars

--- app.vue
+++ app.vue
@@ -1,12 +1,37 @@
 <script setup lang="ts">
+import 'assets/main.scss'
+import variables from 'assets/variables.module.scss';
+
 const website = useWebsiteStore()
 
 await callOnce(website.fetch)
 </script>
 
 <template>
-  <main>
-    <h1>{{ website.name }}</h1>
-    <p>{{ website.description }}</p>
-  </main>
+  <a-config-provider :theme="{
+    token: {
+      colorPrimary: variables.colorPrimary,
+      fontFamily: variables.fontFamily
+    },
+  }">
+    <div class="container">
+      <div class="header">
+        <a-row>
+          <a-col>
+            <div class="logo">
+              Жилфонд
+            </div>
+          </a-col>
+        </a-row>
+      </div>
+      <main>
+        <a-typography-title :level="1">
+          {{ website.name }}
+        </a-typography-title>
+        <a-typography>
+          {{ website.description }}
+        </a-typography>
+      </main>
+    </div>
+  </a-config-provider>
 </template>
