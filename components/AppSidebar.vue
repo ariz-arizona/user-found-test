a05df4295d9570c98b2313b6aa58467a22f15060
aarizona <ar.ariz.arizona@gmail.com>
2024-02-23 15:27:55 +0300

    base css

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -29,35 +29,39 @@ const handleUserClick = (e: number) => {
 }
 </script>
 <template>
-    <a-space direction="vertical" style="width: 100%;">
-        <a-typography-title :level="5">Поиск сотрудников</a-typography-title>
-        <a-input v-model:value="value" placeholder="Введите айди пользователя" @change="onInputChange"
-            :disabled="users.loading" :allow-clear="true">
-            <template #suffix>
-                <span v-if="!isNaN(parseInt(value))">id</span>
-                <span v-else-if="value.length > 3">username</span>
-            </template>
-        </a-input>
+    <a-space direction="vertical" :size="20" style="width: 100%;">
+        <div>
 
-        <a-typography-title :level="5">Результаты</a-typography-title>
-        <a-row v-if="users.loading" align="middle" justify="center" style="min-height: 100px;">
-            <a-col>
-                <a-spin />
-            </a-col>
-        </a-row>
-        <a-typography v-else-if="isEmpty">начните поиск</a-typography>
-        <a-typography v-else-if="value && !users.list.length">ничего не найдено</a-typography>
-        <a-list v-else>
-            <a-list-item v-for="user in users.list">
-                <a-list-item-meta :description="user.email">
-                    <template #title>
-                        <nuxt-link @click="() => handleUserClick(user.id)">{{ user.username }}</nuxt-link>
-                    </template>
-                    <template #avatar>
-                        <a-avatar>{{ user.name[0].toUpperCase() }}</a-avatar>
-                    </template>
-                </a-list-item-meta>
-            </a-list-item>
-        </a-list>
+            <a-typography-title :level="5">Поиск сотрудников</a-typography-title>
+            <a-input v-model:value="value" placeholder="Введите айди пользователя" @change="onInputChange"
+                :disabled="users.loading" :allow-clear="true" size="large">
+                <template #suffix>
+                    <span v-if="!isNaN(parseInt(value))">id</span>
+                    <span v-else-if="value.length > 3">username</span>
+                </template>
+            </a-input>
+        </div>
+        <div>
+            <a-typography-title :level="5">Результаты</a-typography-title>
+            <a-row v-if="users.loading" align="middle" justify="center" style="min-height: 100px;">
+                <a-col>
+                    <a-spin />
+                </a-col>
+            </a-row>
+            <a-typography v-else-if="isEmpty">начните поиск</a-typography>
+            <a-typography v-else-if="value && !users.list.length">ничего не найдено</a-typography>
+            <a-list class="user_list" :split="false" v-else>
+                <a-list-item v-for="user in users.list" class="user_list_item">
+                    <a-list-item-meta :description="user.email">
+                        <template #title>
+                            <nuxt-link @click="() => handleUserClick(user.id)">{{ user.username }}</nuxt-link>
+                        </template>
+                        <template #avatar>
+                            <a-avatar>{{ user.name[0].toUpperCase() }}</a-avatar>
+                        </template>
+                    </a-list-item-meta>
+                </a-list-item>
+            </a-list>
+        </div>
     </a-space>
 </template>
--- components/UserContent.vue
+++ components/UserContent.vue
@@ -13,34 +13,41 @@ users.$onAction(({ name, after }) => {
 })
 </script>
 <template>
-    <a-space direction="vertical" style="width: 100%;">
+    <a-space direction="vertical" style="width: 100%;" class="user">
         <a-row v-if="!users.active" align="middle" justify="center" style="min-height: 50vh;">
             <a-col>
-                <a-typography>Выберите сотрудника, чтобы посмотреть его профиль</a-typography>
+                <a-typography class="user_notfound">Выберите сотрудника, чтобы посмотреть его профиль</a-typography>
             </a-col>
         </a-row>
         <a-row v-else>
-            <a-col span="12">
+            <a-col span="12" class="user_img">
                 <a-image :width="424" :height="286" :preview="false" src="/test.png" fallback="/error.svg" />
             </a-col>
-            <a-col span="12">
-                <a-typography-title :level="5">
-                    {{ active.name }} ({{ active.username }})
-                </a-typography-title>
-                <dl>
-                    <dt>email</dt>
-                    <dd>{{ active.email }}</dd>
-                    <dt>phone</dt>
-                    <dd>{{ active.phone }}</dd>
-                </dl>
-                <a-typography-title :level="5">
-                    О себе
-                </a-typography-title>
-                <a-typography-paragraph>
-                    <a-typography v-for="text in Object.values(active.company)">
-                        {{ text }}
-                    </a-typography>
-                </a-typography-paragraph>
+            <a-col span="12" class="user_content">
+                <a-space direction="vertical" :size="20">
+                    <div>
+
+                        <a-typography-title :level="5">
+                            {{ active.name }} ({{ active.username }})
+                        </a-typography-title>
+                        <dl>
+                            <dt>email</dt>
+                            <dd>{{ active.email }}</dd>
+                            <dt>phone</dt>
+                            <dd>{{ active.phone }}</dd>
+                        </dl>
+                    </div>
+                    <div>
+                        <a-typography-title :level="5">
+                            О себе
+                        </a-typography-title>
+                        <a-typography-paragraph>
+                            <a-typography-text v-for="text in Object.values(active.company)" class="user_about">
+                                {{ text }}
+                            </a-typography-text>
+                        </a-typography-paragraph>
+                    </div>
+                </a-space>
             </a-col>
         </a-row>
     </a-space>
